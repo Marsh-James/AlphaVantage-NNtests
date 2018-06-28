97cbe421d27fd0a38447f2cbc99ad67896521438
James Marsh <jamesfkmarsh@gmail.com>
2018-06-28 19:57:58 +0100

    commented

--- src/crawler/parser.py
+++ src/crawler/parser.py
@@ -1,20 +1,28 @@
 import json
 import os
+# Really basic parser I scrambled out his morning over breakfast. Far from perfect, just get's the job done right now
+# Takes the json file from the crawler and formats the data to something more readable
 
 def parse():
 
     with open(os.path.join('./reuterscrawler/reuterscrawler/spiders/out', "titles.json"), 'r') as f:
+        # Each line in the titles.json file is a json object, so we just take each one and parse it independently
         for line in f:
             data = json.loads(line)
             title = data['title']
             symbol = data['symbol']
             date = data['date']
 
+            # Strip out leading blanks and new line tags
             title = title.lstrip('\n')
             title = title.lstrip(' ')
             title = title.rstrip('| Reuters')
+
+            # Symbols are always found infront of the equals sign in the html data
             symbol = symbol[symbol.find('=') + 1 : len(symbol)]
 
+            # Split date and time into two entities, order of processing makes life easier by splitting things
+            # Relative to '/', as seen in the example unparsed data below
             time = date[date.find('/') + 1: len(date)]
             time = time[0 : time.find('/')]
             time = time.lstrip(' ')
@@ -22,15 +30,10 @@ def parse():
 
             date = date[0 : date.find('/') - 1]
 
-
-
-
-
-
-            save_path = "./parsed/"
-            if not os.path.exists(save_path):
-                os.makedirs(save_path)
-            with open(os.path.join(save_path, "titles.json"), 'a') as out_file:
+            # Dumps into file, does not regenerate file so it must be removed before each startup
+            if not os.path.exists("./parsed/"):
+                os.makedirs("./parsed/")
+            with open(os.path.join("./parsed/", "titles.json"), 'a') as out_file:
                 dump = {'title': title, 'symbol': symbol, 'date': date, 'time': time}
                 print(dump)
                 out = json.dumps(dump)
--- src/crawler/reuterscrawler/reuterscrawler/spiders/scrawler.py
+++ src/crawler/reuterscrawler/reuterscrawler/spiders/scrawler.py
@@ -10,6 +10,15 @@ class ScrawlerSpider(scrapy.Spider):
     def __init__(self, *a, **kw):
         super(ScrawlerSpider, self).__init__(*a, **kw)
 
+    '''
+     Forked and modified code form example crawler I linked in chat before
+     No need rewriting this, just updated it to work on the UK archives rather than US (Which was a pain in its own
+     right.
+     
+     Start_requests, parse_year and parse_day just iterate through the levels of the archive, nothing special.
+     Parse_article looks for the speciifc information in the html like atags and divs with specific classes...
+     Each specific article has its own json object made
+    '''
     def start_requests(self):
         base_url = 'http://www.reuters.com/resources/archive/uk/'
         # years = [2007,2008,2009,2010,2011,2012,2013,2014,2015,2016,2017]
@@ -25,46 +34,37 @@ class ScrawlerSpider(scrapy.Spider):
         for day in days:
             day_url = day.xpath('@href').extract_first()
             date = day_url.split("/")[-1][:-5] # e.g. 20160130
-            item = {'date':date}
-            yield scrapy.Request(url=response.urljoin(day_url), \
-                                 callback=self.parse_day, meta={'item':item})
+            item = {'date': date}
+            yield scrapy.Request(url=response.urljoin(day_url),
+                                 callback=self.parse_day, meta={'item': item})
 
     def parse_day(self, response):
         articles = response.xpath("//div/a[starts-with(@href, \
                               'http://UK.reuters.com/article')]")
 
         for article in articles:
-
             article_link = article.xpath("@href").extract_first()
-
             article_title = article.xpath("text()").extract_first()
-
             item = response.meta['item']
-
             item['title'] = article_title
 
-
-            yield scrapy.Request(url=response.urljoin(article_link), \
-                                 callback=self.parse_article, \
-                                 meta={'item':item})
+            yield scrapy.Request(url=response.urljoin(article_link),
+                                 callback=self.parse_article,
+                                 meta={'item': item})
 
     def parse_article(self, response):
-        atags = response.xpath('//a[contains(@href, "symbol")]/@href').extract_first()
+        symbol = response.xpath('//a[contains(@href, "symbol")]/@href').extract_first()
 
         title = response.xpath('//title/text()').extract_first()
-        formatted_date = response.xpath('//div[@class="date_V9eGk"]/text()').extract_first()
-        #print(title + ' ' + formatted_date + ' ' + atags)
+        date = response.xpath('//div[@class="date_V9eGk"]/text()').extract_first()
 
-
-        if atags is not None:
-            save_path = "./out/"
-            if not os.path.exists(save_path):
-                os.makedirs(save_path)
-            with open(os.path.join(save_path, "titles.json"), 'a') as out_file:
-                article = {
-                            'title': title, 'symbol': atags,
-                            'date': formatted_date
-                          }
+        # Make sure to remove all generated files because I did not include any overwriting.
+        # symbol check just disregards stories without symbols, of which there are many!
+        if symbol is not None:
+            if not os.path.exists("./out/"):
+                os.makedirs("./out/")
+            with open(os.path.join("./out/", "titles.json"), 'a') as out_file:
+                article = {'title': title, 'symbol': symbol, 'date': date}
                 out = json.dumps(article)
                 out_file.write(out+"\n")
 

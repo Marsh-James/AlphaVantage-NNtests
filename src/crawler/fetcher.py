9263563b1d6b68c7a9d6f77aab262102cf1af19c
James Marsh <jamesfkmarsh@gmail.com>
2018-07-05 23:21:41 +0100

    cleanup

--- src/crawler/fetcher.py
+++ src/crawler/fetcher.py
@@ -1,11 +1,23 @@
-from alpha_vantage.timeseries import TimeSeries
-import src.crawler.constants as c
-from pprint import pprint
+from googlefinance.client import get_price_data
+import pandas as pd
 import os
 import json
 
-# Symbol filtering required before this
-ts = TimeSeries(key=c.AV_KEY)
+# LOOKS LIKE WE NEED TO FILTER WEEKEND STORIES AND ONLY KEEP WEEKDAYS!
+
+params = {
+    'q': "UKX",  # Symbol
+    'i': "3600",  # Interval (seconds)
+    'x': "INDEXFTSE",  # Exchange
+    'p': "1Y"  # Period from today (d, Y)
+}
+
+historical_data = get_price_data(params) # Updated local wrapper version to also return index as datetime
+json_port = historical_data.to_json(orient='index')
+
+print(json_port) # Starts furthest back works towards present (9 records in a day)
+# print(historical_data)
+
 with open(os.path.join('./parsed', "titles.json"), 'r') as f:
     # Each line in the titles.json file is a json object, so we just take each one and parse it independently
     for line in f:
@@ -14,16 +26,16 @@ with open(os.path.join('./parsed', "titles.json"), 'r') as f:
         symbol = data['symbol']
         date = data['date']
         time = data['time']
-        market_open_time = '10:00:00' # From data point's im getting, this is the shortest time I can get. Eastern time
-        next_time_interval = '11:00:00' # Assuming adding 1hr tracking time
 
-        historical_data, meta_data = ts.get_intraday(symbol=symbol, interval='60min', outputsize='full')
+        market_open_time = '08:00:00'  # From data point's im getting, this is the shortest time I can get. Eastern time
+        next_time_interval = '09:00:00'  # Assuming adding 1hr tracking time
+
         # Dates & times do not match format but it's just a rejig
         if historical_data[date + ' ' + market_open_time] is not None:
             result = None
 
-            if historical_data[date + ' ' + next_time_interval]['4. close'] > \
-                    historical_data[date + ' ' + market_open_time]['4. close']:
+            if historical_data[date + ' ' + next_time_interval]['close'] > \
+                    historical_data[date + ' ' + market_open_time]['close']:
                 result = 1
             else:
                 result = 0

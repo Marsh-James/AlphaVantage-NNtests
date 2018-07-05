65e78260c733109baa40415eb20f82f79298ce7d
James Marsh <jamesfkmarsh@gmail.com>
2018-07-05 23:19:30 +0100

    added formatter

--- src/crawler/format.py
+++ src/crawler/format.py
@@ -5,7 +5,9 @@ import datetime
 # Really basic parser I scrambled out his morning over breakfast. Far from perfect, just get's the job done right now
 # Takes the json file from the crawler and formats the data to something more readable
 
-def filter_parsed():
+def format_parsed():
+    with open(os.path.join("./parsed/", "filtered_titles.json"), 'w') as init:
+        init.write(json.dumps({}))
 
     with open(os.path.join('./parsed/', "titles.json"), 'r') as f:
         # Each line in the titles.json file is a json object, so we just take each one and parse it independently
@@ -25,13 +27,18 @@ def filter_parsed():
             # Dumps into file, does not regenerate file so it must be removed before each startup
             if not os.path.exists("./parsed/"):
                 os.makedirs("./parsed/")
-            with open(os.path.join("./parsed/", "filtered_titles.json"), 'a') as out_file:
-                dump = {'title': title, 'symbol': symbol, 'date': date, 'time': time}
-                print(dump)
-                out = json.dumps(dump)
-                out_file.write(out+"\n")
+            with open(os.path.join("./parsed/", "filtered_titles.json"), 'r+') as output:
+                new_obj = json.loads(output.read())
+                new_obj[title] = {
+                    "symbol": symbol,
+                    "date": date,
+                    "time": time
+                }
+                print(new_obj)
+                output.seek(0)
+                json.dump(new_obj, output)
 
-filter_parsed()
+format_parsed()
     #{
     #    "title": "\n                Exclusive: U.S. may soon claim up to $1.7 billion penalty from China's ZTE  - sources | Reuters",
     #    "symbol": "/business/quotes/overview?symbol=000063.SZ", "date": "June 1, 2018 /  8:02 PM / a month ago"}

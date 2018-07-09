ecedf3a7647609066db413d82974f4efb9c8c7c0
Chris Wang <chris_wang@icloud.com>
2018-07-09 22:17:21 +0100

    integrated firebase

--- src/crawler/fetcher.py
+++ src/crawler/fetcher.py
@@ -1,9 +1,18 @@
 from googlefinance.client import get_price_data
+import firebase_admin
+from firebase_admin import credentials, db
 import json
 import os
 
+cred = credentials.Certificate('firebase_key.json')
+default_app = firebase_admin.initialize_app(cred, {
+    'databaseURL': 'https://newsnet-cdf13.firebaseio.com'
+})
 
 def fetch():
+    with open(os.path.join("./parsed/", "training_data.json"), 'w') as init:
+        init.write(json.dumps({}))
+
     data = json.load(open('./parsed/filtered_titles.json'))
 
     for majorkey, subdict in data.items():
@@ -29,7 +38,6 @@ def fetch():
 
             # Time key to verify day open existence
             time_key = str(date) + ' ' + str(market_open_time)
-            print(time_key)
             if time_key in json_port:
 
                 if int(json_port[date + ' ' + next_time_interval]['Close']) > int(json_port[date + ' ' + market_open_time]['Open']) * 1.03:
@@ -38,11 +46,15 @@ def fetch():
                     result = 0
                 if not os.path.exists("./final/") and result is not None:
                     os.makedirs("./final/")
-                with open(os.path.join("./parsed/", "training_data.json"), 'a') as out_file:
-                    dump = {'title': title, 'result': result}
-                    print(dump)
-                    out = json.dumps(dump)
-                    out_file.write(out+"\n")
+                with open(os.path.join("./parsed/", "training_data.json"), 'r+') as output:
+                    new_obj = json.loads(output.read())
+                    new_obj[title] = result
+                    output.seek(0)
+                    print(new_obj)
+                    json.dump(new_obj, output)
+                db.reference('/').update({
+                    title: result
+                })
         except:
             print('Oops, something went wrong!')
 
